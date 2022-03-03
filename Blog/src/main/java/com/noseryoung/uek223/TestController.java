46d854cf9ad0007389b6e522d5362337ac3d9849
Davide Marcoli <davide@marcoli.ch>
2022-03-03 11:36:57 +0100

    Update Swagger Properties Add GlobalExceptionHandler

--- Blog/src/main/java/com/noseryoung/uek223/TestController.java
+++ Blog/src/main/java/com/noseryoung/uek223/TestController.java
@@ -1,12 +1,13 @@
 package com.noseryoung.uek223;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/test")
+@Hidden
 public class TestController {
-
     @RequestMapping("/hello")
     public String hello() {
         return "Hello World!";
---
+++ Blog/src/main/java/com/noseryoung/uek223/domain/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.noseryoung.uek223.domain.exceptions;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import org.springframework.security.access.AccessDeniedException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler({AccessDeniedException.class})
+    protected ResponseEntity<String> handleAccessDeniedAxception(AccessDeniedException ex, WebRequest request) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        return new ResponseEntity<>("Your user \"" + username + "\" doesn't have access to this ressource!", new HttpHeaders(), HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    protected ResponseEntity<String> handleUnauthorized(AuthenticationException ex, WebRequest request) {
+        return new ResponseEntity<>("Please enter the correct login details!", new HttpHeaders(), HttpStatus.UNAUTHORIZED);
+    }
+}
